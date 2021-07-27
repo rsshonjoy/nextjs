145a7d4557d62456c5d4da196cab6a75cd4aa7dc
rsshonjoy <rsshonjoydas371819@gmail.com>
2021-07-28 05:32:25 +0600

    Incremental Static Generation

---
+++ pages/products/[productId].js
@@ -0,0 +1,41 @@
+import { useRouter } from "next/router"
+
+const Product = ({ product }) => {
+  const router = useRouter()
+
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <div>
+      <h2>
+        {product.id} {product.title} {product.price}
+      </h2>
+      <p>{product.description}</p>
+      <hr/>
+    </div>
+  )
+}
+
+export default Product
+
+export async function getStaticProps(context) {
+  const {params} = context
+  const res = await fetch(`http://localhost:4000/products/${params.productId}`)
+  const data = await res.json()
+  console.log(`Generation page for /products/${params.productId}`);
+
+  return {
+    props: {
+      product: data
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: {productId: '1' }}],
+    fallback: true
+  }
+}
---
+++ pages/products/index.js
@@ -0,0 +1,30 @@
+const ProductList = ({ products }) => {
+  return (
+    <>
+      <h1>List of Products</h1>
+      {products.map(product => {
+        return (
+          <div key={product.id}>
+            <h2>
+              {product.id} {product.title} {product.price}
+            </h2>
+            <hr/>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+export default ProductList
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:4000/products')
+  const data = await res.json();
+
+  return {
+    props: {
+      products: data,
+    }
+  }
+}
